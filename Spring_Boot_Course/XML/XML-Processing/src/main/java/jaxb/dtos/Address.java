637a552ff861b690d22436fc431e29c94b0bb224
Kristian Popov <kristianpopov03@yahoo.com>
2025-08-06 10:51:45 +0200

    XML

---
+++ Spring_Boot_Course/XML/XML-Processing/src/main/java/jaxb/dtos/Address.java
@@ -0,0 +1,20 @@
+package jaxb.dtos;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Address {
+    private String country;
+    private String city;
+
+    public Address() {}
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
